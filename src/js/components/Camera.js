5607c6c9374cc3aa2026a0de34f319e9cae6feff
Andras Csepreghy <andris788@gmail.com>
2016-04-19 18:49:18 +0200

    Camera angle adjust

--- src/js/components/Camera.js
+++ src/js/components/Camera.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default props => (
   <Entity position={[10, 0, 20]}
-          rotation={[-15, 10, 0]}>
+          rotation={[0, 0, 0]}>
     <Entity camera=""
             look-controls=""
             wasd-controls={{enabled: true}} />
