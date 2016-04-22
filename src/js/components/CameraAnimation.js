a4b0a84af7c85ca8349b75b9c4d77135a4a20081
Andras Csepreghy <andris788@gmail.com>
2016-04-23 01:32:27 +0200

    Change a-animation to Animation

--- src/js/components/CameraAnimation.js
+++ src/js/components/CameraAnimation.js
@@ -1,16 +1,16 @@
-import {Entity} from 'aframe-react';
+import {Entity, Animation} from 'aframe-react';
 import React from 'react';
 
 export default class CameraAnimation extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { newPosition:  "0 0 0" };
+    this.state = { newPosition:  [0, 0, 0] };
   }
 
   render() {
     return (
-          <a-animation
+          <Animation
               attribute="position"
               dur="4000"
               to={ this.state.newPosition }
