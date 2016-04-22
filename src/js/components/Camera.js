8af06c13a279fe2570793d7dce30f8e73603e062
Andras Csepreghy <andris788@gmail.com>
2016-04-23 00:45:54 +0200

    Animation working on camera

--- src/js/components/Camera.js
+++ src/js/components/Camera.js
@@ -1,11 +1,25 @@
 import {Entity} from 'aframe-react';
 import React from 'react';
 
-export default props => (
-  <Entity position={[10, 0, 20]}
-          rotation={[0, 0, 0]}>
-    <Entity camera=""
-            look-controls=""
-            wasd-controls={{enabled: true}} />
-  </Entity>
-);
+export default class Camera extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+    render() {
+      return (
+          <Entity position={[10, 0, 20]}
+                  rotation={[0, 0, 0]}>
+            <a-animation
+                attribute="position"
+                dur="4000"
+                to="0 0 0"
+            />
+            <Entity camera=""
+                    look-controls=""
+                    wasd-controls={{enabled: true}}>
+            </Entity>
+          </Entity>
+      );
+    }
+}
