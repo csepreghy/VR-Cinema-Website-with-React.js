50928733afda17fb08ae59470003db6708dfc1d0
Andras Csepreghy <acsepreghy@ganintegrity.com>
2016-08-11 00:08:10 +0200

    Added some new models

--- src/js/components/Assets.js
+++ src/js/components/Assets.js
@@ -11,8 +11,11 @@ export default class Assets extends React.Component {
     return (
         <a-assets>
 
-          <a-asset-item id="cinema-obj" src="assets/models/Grand-Teatret.obj"></a-asset-item>
-          <a-asset-item id="cinema-mtl" src="assets/models/Grand-Teatret.mtl"></a-asset-item>
+          <a-asset-item id="cinema-obj" src="assets/models/grand-teatret.obj"></a-asset-item>
+          <a-asset-item id="cinema-mtl" src="assets/models/grand-teatret.mtl"></a-asset-item>
+
+          <a-asset-item id="cube-obj" src="assets/models/unwrap_test.obj"></a-asset-item>
+          <a-asset-item id="cube-mtl" src="assets/models/unwrap_test.mtl"></a-asset-item>
 
           <img id="back" src="assets/img/back-icon.png"></img>
           <img id="book-seat" src="assets/img/book-seat-icon.png"></img>
