fe4955e3c589f819dfd79a25e04c330714ace57f
Andras Csepreghy <acsepreghy@ganintegrity.com>
2016-08-10 23:44:10 +0200

    Changed video size to fit movie canvas

--- src/js/components/Movie.js
+++ src/js/components/Movie.js
@@ -2,7 +2,7 @@ import {Entity} from 'aframe-react';
 import React from 'react';
 
 export default class Movie extends React.Component {
-  
+
   constructor(props) {
     super(props);
 
@@ -12,11 +12,12 @@ export default class Movie extends React.Component {
     return (
         <Entity>
           <a-video
+              webkit-playsinline=""
               id="movie"
               src="#movie"
-              width="90"
-              height="48"
-              position="0 40 -154" />
+              width="85.7"
+              height="46.7"
+              position="0 40 -163.25" />
         </Entity>
     );
   }}
